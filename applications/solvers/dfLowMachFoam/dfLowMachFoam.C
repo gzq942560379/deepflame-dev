36dd3e493bf8596c13390acd6992923543ff2289
maorz1998 <maorz1998@stu.pku.edu.cn>
2023-09-13 20:42:27 +0800

    CH4

--- applications/solvers/dfLowMachFoam/dfLowMachFoam.C
+++ applications/solvers/dfLowMachFoam/dfLowMachFoam.C
@@ -67,11 +67,11 @@ Description
 
 // #define _CSR_
 // #define _ELL_
-#define _DIV_
+// #define _DIV_
 // #define _LDU_
-#define OPT_GenMatrix_Y
-#define OPT_GenMatrix_E
-#define OPT_GenMatrix_U
+// #define OPT_GenMatrix_Y
+// #define OPT_GenMatrix_E
+// #define OPT_GenMatrix_U
 // #define OPT_GenMatrix_p
 // #define OPT_GenMatrix_U_check
 // #define OPT_GenMatrix_Y_check
