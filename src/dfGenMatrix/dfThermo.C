d1563d6e49ea86e26c32ed6b4aef59f1ef6173c3
maorz1998 <maorz1998@stu.pku.edu.cn>
2024-01-16 21:22:20 +0800

    YEqn

--- src/dfGenMatrix/dfThermo.C
+++ src/dfGenMatrix/dfThermo.C
@@ -9,16 +9,36 @@ double *nasa_coeffs, *viscosity_coeffs, *conductivity_coeffs, *binary_diffusion_
         *viscosity_constant1, *viscosity_constant2;
 
 int nSpecies = 0, nBoundarySurfaces = 0, nBoundaryPatches = 0;
-Foam::label nCells = 0;
+Foam::label nCells = 0, nFaces = 0, nProcessBoundarySurfaces = 0;
+Foam::label *surfacePerPatch;
+int *neighbProcNo;
 
 void init_const_coeff_ptr(std::string mechanism_file, Foam::PtrList<Foam::volScalarField>& Y)
 {
     nSpecies = Y.size();
     nCells = Y[0].size();
+    const Foam::fvMesh& mesh = Y[0].mesh();
+    const Foam::labelUList& neighbour = mesh.neighbour();
+    nFaces = neighbour.size();
+
     forAll(Y[0].boundaryField(), patchi) {
         const Foam::fvPatchScalarField& patchYi = Y[0].boundaryField()[patchi];
         nBoundaryPatches ++;
         nBoundarySurfaces += patchYi.size();
+        if (patchYi.type() == "processor" || patchYi.type() == "processorCyclic") 
+            nProcessBoundarySurfaces += 2 * patchYi.size();
+        else 
+            nProcessBoundarySurfaces += patchYi.size();
+    }
+    neighbProcNo = new int[nBoundaryPatches];
+    surfacePerPatch = new Foam::label[nBoundaryPatches];
+    std::fill(neighbProcNo, neighbProcNo + nBoundaryPatches, -1);
+    forAll(Y[0].boundaryField(), patchi) {
+        const Foam::fvPatchScalarField& patchYi = Y[0].boundaryField()[patchi];
+        surfacePerPatch[patchi] = patchYi.size();
+        if (patchYi.type() == "processor" || patchYi.type() == "processorCyclic") {
+            neighbProcNo[patchi] = dynamic_cast<const Foam::processorFvPatchField<Foam::scalar>&>(patchYi).neighbProcNo();
+        } 
     }
 
     nasa_coeffs = new double[15 * nSpecies];
@@ -428,9 +448,9 @@ void correctThermo
 
     // calculate rhoD
     // TODO: only support UnityLewis for now
-    for (int i = 0; i < nBoundarySurfaces; ++i) {
-        for (int j = 0; j < nSpecies; ++j) {
-            boundary_rhoD[i * nSpecies + j] = boundary_alpha[i];
+    for (int i = 0; i < nSpecies; ++i) {
+        for (int j = 0; j < nBoundarySurfaces; ++j) {
+            boundary_rhoD[i * nBoundarySurfaces + j] = boundary_alpha[j];
         }
     }
 
