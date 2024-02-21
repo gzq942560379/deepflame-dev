50ef4f8b8a6b60ca92fd6ae00210b5ee374975dd
fengguofeng <gzq9425@qq.com>
2024-02-21 10:55:42 +0900

    BLASDNN CMakeList

--- install.sh
+++ install.sh
@@ -27,29 +27,58 @@ if [ ! -z "$LIBTORCH_ROOT"  ]; then
     cd "$DF_SRC/dfChemistryModel/DNNInferencer"
     mkdir build
     cd build
-    # # x86_64
-    # cmake ..
-    # fugaku
-    cmake .. -DCMAKE_CXX_COMPILER=mpiFCC -DCMAKE_CXX_FLAGS="-Nclang -Ofast -g -mlittle-endian" -DLIBTENSORFLOW_ROOT=$LIBTENSORFLOW_ROOT
+    cmake ..
     cp ./libDNNInferencer.so $DF_ROOT/lib/
 fi
 if [ ! -z "$USE_BLASDNN" ]; then
-    cd "$DF_SRC/dfChemistryModel/DNNInferencer_blas"
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_CXX_COMPILER=mpiFCC -DCMAKE_CXX_FLAGS="-Nclang -Ofast -g -fopenmp -Nfjomplib -std=c++11"
-    if [ $? -ne 0 ]; then
-        echo "Error: CMake configuration failed. Exiting installation."
-        cd $DF_ROOT
-        return 1
+    if [ ! -z "$USE_FUGAKU" ]; then
+        cd "$DF_SRC/dfChemistryModel/DNNInferencer_blas"
+        mkdir build
+        cd build
+        cmake .. \
+            -DCMAKE_CXX_COMPILER=mpiFCC \
+            -DCMAKE_CXX_FLAGS="-Nclang -Ofast -g -fopenmp -Nfjomplib -std=c++11" \
+            -DYAML_INCLUDE=/vol0001/hp230257/guozhuoqiang/DeepFlame/software/yaml-cpp-0.7.0/include/yaml-cpp \
+            -DYAML_LIBRARY=/vol0001/hp230257/guozhuoqiang/DeepFlame/software/yaml-cpp-0.7.0/lib64/libyaml-cpp.so \
+            -DBLAS_LIBRARY=fjlapackexsve
+
+        if [ $? -ne 0 ]; then
+            echo "Error: CMake configuration failed. Exiting installation."
+            cd $DF_ROOT
+            return 1
+        fi
+        make VERBOSE=1
+        if [ $? -ne 0 ]; then
+            echo "Error: Compilation failed. Exiting installation."
+            cd $DF_ROOT
+            return 1
+        fi
+        cp ./libDNNInferencer_blas.so $DF_ROOT/lib/
+    else 
+        cd "$DF_SRC/dfChemistryModel/DNNInferencer_blas"
+        mkdir build
+        cd build
+        cmake .. \
+            -DCMAKE_CXX_COMPILER=mpiCC \
+            -DCMAKE_CXX_FLAGS="-Ofast -g -fopenmp -std=c++11" \
+            -DYAML_INCLUDE=/root/MassiveParallel/deepflame-dev/yaml-cpp/include \
+            -DYAML_LIBRARY=/root/MassiveParallel/deepflame-dev/yaml-cpp/build/libyaml-cpp.a \
+            -DBLAS_LIBRARY=""
+
+        if [ $? -ne 0 ]; then
+            echo "Error: CMake configuration failed. Exiting installation."
+            cd $DF_ROOT
+            return 1
+        fi
+        make VERBOSE=1
+        if [ $? -ne 0 ]; then
+            echo "Error: Compilation failed. Exiting installation."
+            cd $DF_ROOT
+            return 1
+        fi
+        cp ./libDNNInferencer_blas.so $DF_ROOT/lib/
     fi
-    make VERBOSE=1
-    if [ $? -ne 0 ]; then
-        echo "Error: Compilation failed. Exiting installation."
-        cd $DF_ROOT
-        return 1
-    fi
-    cp ./libDNNInferencer_blas.so $DF_ROOT/lib/
 fi
+
 cd $DF_ROOT
 ./Allwmake -j && print_finish
