b96291a2855648d9209fd21df6b6141332efbc0f
maorz1998 <maorz1998@stu.pku.edu.cn>
2024-03-05 16:38:36 +0800

    MassiveParallel

--- applications/solvers/dfLowMachFoam/dfLowMachFoam.C
+++ applications/solvers/dfLowMachFoam/dfLowMachFoam.C
@@ -68,12 +68,12 @@ Description
 #include "GenFvMatrix.H"
 #include "multivariateGaussConvectionScheme.H"
 
-// #define _CSR_
+#define _CSR_
 // #define _ELL_
-#define _DIV_
+// #define _DIV_
 // #define _LDU_
 #define OPT_GenMatrix_Y
-// #define OPT_GenMatrix_E
+#define OPT_GenMatrix_E
 #define OPT_GenMatrix_U
 #define OPT_GenMatrix_p
 // #define OPT_thermo
